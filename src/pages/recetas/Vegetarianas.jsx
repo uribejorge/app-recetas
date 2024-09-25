622c823710b40d6a79bb5a085333424db714fa93
uribejorge <jorge.uribe112180@gmail.com>
2024-09-24 19:57:08 -0500

    c1

--- src/pages/recetas/Vegetarianas.jsx
+++ src/pages/recetas/Vegetarianas.jsx
@@ -1,16 +1,23 @@
+import { Link } from "react-router-dom"
 import { recetasVegetarianas } from "../../data/recetas"
+import './vegetarianas.css'
 
 const Vegetarianas = () => {
   return (
-    <div>
+    <div className="cards">
       {
         recetasVegetarianas.map((receta) => (
-          <section>
+          <section key={receta.id}>
             {/* <p>Id: {receta.id}</p> */}
             <p>Nombre: {receta.nombre}</p>
             <p>Descripcion: {receta.descripcion}</p>
             <p>Dificultad: {receta.dificultad}</p>
             <p>Tiempo: {receta.tiempo}</p>
+            <div>
+              <button>Eliminar</button>
+              <Link>Editar</Link>
+              <Link>Detalles</Link>
+            </div>
           </section>
         ))
       }
