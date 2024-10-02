583865c968947f94cfd700b775a40418dbc06036
uribejorge <jorge.uribe112180@gmail.com>
2024-10-01 20:55:44 -0500

    ultimo cambio

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,25 +1,39 @@
+import { useState } from "react";
 import React from "react";
 import './Login.css';
 
 
-
-
-
 const Login = () => {
+    const [usuario, setUsuario] = useState('')
+    const [Contraseña, setContraseña] = useState('')
+    const [Correo, setCorreo] = useState('')
+
+    console.log(usuario)
+    function signIn() {
+        if (usuario == 'jorge' && Contraseña == '123456') {
+            alert('inicio de sesion correcto')
+        }
+
+    }
     return (
         <form className="form" action="">
             <section className="">
                 <div>
                     <label htmlFor="usuario">Usuario</label>
-                    <input id="usuario" type="text" />
+                    <input onChange={(e) => { setUsuario(e.target.value) }}
+                        id="usuario" type="text" />
                 </div>
                 <br />
                 <div>
                     <label htmlFor="password">Contraseña</label>
-                    <input id="password" type="text" />
+                    <input onChange={(e) => { setContraseña(e.target.value) }}
+                        id="password" type="text" />
                 </div>
+                <label htmlFor="password">Contraseña</label>
+                    <input onChange={(e) => { setCorreo(e.target.value) }}
+                        id="password" type="text" />
                 <br />
-                <button className="button" type="button" >Iniciar sesión</button>
+                <button onClick={signIn} className="button" type="button" >Iniciar sesión</button>
             </section>
         </form>
     )
